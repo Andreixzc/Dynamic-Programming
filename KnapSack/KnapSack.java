49a5a6dd0c8dcf82dd5a3dde583305455580fb5c
Andreixzc <andrei.massaini@hotmail.com>
2023-11-04 13:41:46 -0300

    knapsack tabulation attemp

--- KnapSack/KnapSack.java
+++ KnapSack/KnapSack.java
@@ -5,7 +5,8 @@ public class KnapSack {
     public static int[] weights = { 10, 20, 30 }; // Pesos dos itens
 
     public static void main(String[] args) {
-        System.out.println(knapSackMemo(50, values.length));
+        // System.out.println(knapSackMemo(50, values.length));
+        System.out.println(knapSackTabulation(5, values.length));
     }
 
     public static int knapSackMemo(int cap, int n) {
@@ -30,6 +31,29 @@ public class KnapSack {
             return Math.max(pegar, passar);
         }
         return memo[n][cap];
+    }
+
+    public static int knapSackTabulation(int cap, int n) {
+        int[][] dp = new int[values.length + 1][cap + 1];
+        return knapSackTabulation(dp, cap, 0);
+    }
+
+    public static int knapSackTabulation(int[][] dp, int cap, int n) {
+
+        for (int i = 0; i <= dp.length-1; i++) {
+            for (int j = 0; j <= cap; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (weights[i] <= cap) {
+                    int pegar = dp[i - 1][j];
+                    int passar = values[i] + dp[i - 1][j - weights[i]];
+                    dp[i][j] = Math.max(pegar, passar);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[values.length][cap];
     }
 
     public static void fillMemo(int[][] memo) {
