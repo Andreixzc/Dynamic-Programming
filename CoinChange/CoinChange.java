48d22780756e05ef80071582506408445c8b61c1
Andreixzc <andrei.massaini@hotmail.com>
2023-11-06 10:15:01 -0300

    coin change scratch

---
+++ CoinChange/CoinChange.java
@@ -0,0 +1,39 @@
+package CoinChange;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1); // Initialize dp array with a value greater than amount
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    int melhorAtual = dp[i];
+                    int possivelMelhor = dp[i - coins[j]] + 1;
+                    dp[i] = Math.min(melhorAtual, possivelMelhor);
+                    //basicamente, dp[i] = dp[moedaAtual - trocoAtual] +1
+                }
+            }
+        }
+
+        if (dp[amount] > amount) {
+            return -1;
+        }
+        return dp[amount];
+
+    }
+
+
+}
+
+
